5a67e7556cf708fc08b6df8739a3a050be98f5e7
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-01-30 20:20:14 -0700

    updated projects to most current projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,11 +4,20 @@ export default function Projects() {
       <div className="projects">
         <a
           className="project-item"
-          href="https://thenhlproject.herokuapp.com/"
+          href="https://gamesauce-project.herokuapp.com/"
           target="_blank"
           rel="noreferrer"
         >
-          <div className="project-title">IceTime Application</div>
+          <div className="project-title">GameSauce</div>
+          <div className="project-info">2023 / Project</div>
+        </a>
+        <a
+          className="project-item"
+          href="https://golden-goose-finance.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="project-title">Golden Goose Finance</div>
           <div className="project-info">2023 / Project</div>
         </a>
         <a
@@ -27,15 +36,6 @@ export default function Projects() {
           rel="noreferrer"
         >
           <div className="project-title">WEBABOOS Application</div>
-          <div className="project-info">2022 / Project</div>
-        </a>
-        <a
-          className="project-item"
-          href="https://jerimiahk.github.io/jerimiahsCodeQuiz/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="project-title">DRUM QUIZ Application</div>
           <div className="project-info">2022 / Project</div>
         </a>
       </div>
